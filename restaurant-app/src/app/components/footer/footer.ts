a7f0e4eb205b1611c3f760e4ac543577bb4bc424
OtadTOAD <90140046+OtadTOAD@users.noreply.github.com>
2025-08-13 18:42:19 +0400

    Added links to footer

--- restaurant-app/src/app/components/footer/footer.ts
+++ restaurant-app/src/app/components/footer/footer.ts
@@ -1,11 +1,12 @@
 import { TranslocoPipe } from '@ngneat/transloco';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-footer',
-  imports: [CardModule, ButtonModule, TranslocoPipe],
+  imports: [CardModule, ButtonModule, TranslocoPipe, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
