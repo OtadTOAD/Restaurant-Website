3fccf1eb037213de02e2a0bb2258d08778ee2105
OtadTOAD <90140046+OtadTOAD@users.noreply.github.com>
2025-08-13 16:47:10 +0400

    prototype header

--- restaurant-app/src/app/components/header/header.ts
+++ restaurant-app/src/app/components/header/header.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  menuItems: MenuItem[] = [
+    {
+      label: 'About Us'
+    },
+    {
+      label: 'Contact Us'
+    },
+    {
+      label: 'Language',
+      items: [
+        { label: 'English' },
+        { label: 'ქართული' },
+        { label: 'Русский' }
+      ]
+    }
+  ];
 
+  constructor() {
+
+  }
 }
