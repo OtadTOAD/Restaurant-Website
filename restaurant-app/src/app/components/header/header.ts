fa771cbb1d9d82d7cadbafb5135054ccfa14ca66
Askvernitel <danielkolotashvilio@gmail.com>
2025-08-15 02:05:34 +0400

    Added hamburger in nav bar fixed colors and i18n dropdown

--- restaurant-app/src/app/components/header/header.ts
+++ restaurant-app/src/app/components/header/header.ts
@@ -1,18 +1,19 @@
 import { TranslocoService, TranslocoPipe } from '@ngneat/transloco';
 import { RouterLink } from '@angular/router';
 import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { CommonModule, NgStyle } from '@angular/common';
 import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, NgStyle, TranslocoPipe],
+  imports: [RouterLink, NgStyle, TranslocoPipe, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  hamburgerOpen = false;
   @ViewChild('langBtn', { read: ElementRef }) langBtn!: ElementRef<HTMLButtonElement>;
-  dropdownPos = { top: 0, left: 0 };
+  dropdownPos = { top: 0, left: 0, width: 0 };
   dropdownOpen = false;
 
   constructor(private translocoService: TranslocoService) {
@@ -35,7 +36,8 @@ export class Header {
     const rect = this.langBtn.nativeElement.getBoundingClientRect();
     this.dropdownPos = {
       top: rect.bottom + window.scrollY,
-      left: rect.left + window.scrollX
+      left: rect.left + window.scrollX,
+      width: rect.width,
     };
   }
   @HostListener('document:scroll', ['$event'])
@@ -61,4 +63,13 @@ export class Header {
   setLang(lang: string) {
     this.translocoService.setActiveLang(lang)
   }
+
+  handleHamburgerClick() {
+    if (this.hamburgerOpen) {
+      this.hamburgerOpen = false;
+    } else {
+      this.hamburgerOpen = true;
+    }
+  }
+
 }
