e5bae0bc0c802592451ea7d65c9bd6f33be12db3
OtadTOAD <90140046+OtadTOAD@users.noreply.github.com>
2025-08-14 12:40:41 +0400

    Made website more responsive and better

--- restaurant-app/src/app/components/header/header.ts
+++ restaurant-app/src/app/components/header/header.ts
@@ -1,15 +1,19 @@
 import { TranslocoService, TranslocoPipe } from '@ngneat/transloco';
 import { RouterLink } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
+import { NgStyle } from '@angular/common';
 import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, NgStyle],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  @ViewChild('langBtn', { read: ElementRef }) langBtn!: ElementRef<HTMLButtonElement>;
+  dropdownPos = { top: 0, left: 0 };
+  dropdownOpen = false;
 
   constructor(private translocoService: TranslocoService) {
     combineLatest([
@@ -18,6 +22,40 @@ export class Header {
       this.translocoService.selectTranslate('LANGUAGE')
     ]).subscribe(([aboutLabel, contactLabel, langLabel]) => {
     });
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+    if (this.dropdownOpen) {
+      this.updateDropdownPosition();
+    }
+  }
+
+  updateDropdownPosition() {
+    const rect = this.langBtn.nativeElement.getBoundingClientRect();
+    this.dropdownPos = {
+      top: rect.bottom + window.scrollY,
+      left: rect.left + window.scrollX
+    };
+  }
+  @HostListener('document:scroll', ['$event'])
+  onScroll(event: Event) {
+    this.dropdownOpen = false;
+  }
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (
+      this.dropdownOpen &&
+      !this.langBtn.nativeElement.contains(event.target as Node)
+    ) {
+      this.dropdownOpen = false;
+    }
+  }
+  @HostListener('window:resize')
+  onResize() {
+    if (this.dropdownOpen) {
+      this.updateDropdownPosition();
+    }
   }
 
   setLang(lang: string) {
--- restaurant-app/src/app/components/landing/landing.ts
+++ restaurant-app/src/app/components/landing/landing.ts
@@ -1,6 +1,6 @@
 import { TranslocoPipe } from '@ngneat/transloco';
 import { RouterLink } from '@angular/router';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy } from '@angular/core';
 
 
 @Component({
@@ -9,4 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './landing.html',
   styleUrl: './landing.css'
 })
-export class Landing { }
+export class Landing implements AfterViewInit { 
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.initIntersectionObserver();
+    }, 50);
+  }
+
+  initIntersectionObserver() {
+    const observerOptions = {
+      threshold: 0.1,
+      rootMargin: '0px 0px -50px 0px'
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        } else {
+          entry.target.classList.remove('animate');
+        }
+      });
+    }, observerOptions);
+
+    const animatedElements = document.querySelectorAll(
+      '.fade-in-up, .fade-in-left, .fade-in-right, .scale-in'
+    );
+    animatedElements.forEach(el => observer.observe(el));
+  }
+}
