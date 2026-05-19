6f4a89bce52e590cd8fb5caef16ee3e569d4246f
OtadTOAD <90140046+OtadTOAD@users.noreply.github.com>
2026-05-19 22:52:15 +0400

    feat: idk man

---
+++ restaurant-app/src/app/components/terms-conditions/terms-conditions.ts
@@ -0,0 +1,12 @@
+import { TranslocoPipe } from '@ngneat/transloco';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-conditions',
+  imports: [TranslocoPipe],
+  templateUrl: './terms-conditions.html',
+  styleUrl: './terms-conditions.css'
+})
+export class TermsConditions {
+
+}
